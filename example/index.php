19bb5c6f2a46019daf3cc91831fec40cb07c774d
Miraç Şengönül <miracsengonul@gmail.com>
2017-08-30 10:36:06 +0300

    Dosya Eklenmesi

---
+++ example/index.php
@@ -0,0 +1,104 @@
+<?php
+
+require_once('Form.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">KolayForm</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Anasayfa</a></li>
+            <li><a href="#">Sayfa 1</a></li>
+            <li><a href="#">Sayfa 2</a></li>
+            <li><a href="#">Sayfa 3</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="col-md-12">
+
+
+
+    <?php echo Form::open('google.com','POST',NULL); ?>
+    <div class="form-group">
+        <?php
+        echo Form::label('İsminizi Giriniz','isim');
+        echo Form::input([
+            'type'=>'text',
+            'name'=>'name',
+            'id'=> 'isim',
+            'placeholder'=>'Lütfen isminizi yazınız.',
+            'class'=>'form-control'
+        ]);
+        ?>
+    </div>
+
+    <div class="form-group">
+        <?php
+        echo Form::label('Cinsiyet','cinsiyet');
+        echo Form::select('cinsiyet',
+            [
+                '1'=>'Erkek',
+                '2'=>'Kadın'
+            ]
+            ,
+            [
+                'class'=>'form-control',
+                'id'=>'cinsiyet'
+            ]);
+        ?>
+    </div>
+
+    <div class="form-group">
+        <?php
+        echo Form::label('Parola','parola');
+        echo Form::pass('parola',['class'=>'form-control','placeholder'=>'Parolanızı girin.','id'=>'parola']);
+        ?>
+    </div>
+
+
+    <div class="form-group">
+        <?php
+        echo Form::label('Kendinizden Bahsedin','bahsedin');
+        echo Form::textarea('about',['class'=>'form-control','placeholder'=>'Kendinizden biraz bahsedebilir misiniz ?.','id'=>'bahsedin']);
+        ?>
+    </div>
+
+
+    <div class="form-group">
+        <?php
+        echo Form::label('Doğum Tarihi','dogum_tarihi');
+        echo Form::date('dogum_tarihi',['class'=>'form-control','placeholder'=>'Doğum Tarihiniz','id'=>'dogum_tarihi']);
+        ?>
+    </div>
+
+
+    <div class="form-group">
+        <?php
+        echo Form::label('Mail Adresi','mail');
+        echo Form::date('mail',['class'=>'form-control','placeholder'=>'Mail adresiniz','id'=>'mail']);
+        ?>
+    </div>
+
+
+    <div class="form-group">
+        <?php echo Form::submit('gonder','Gönder',['class'=>'form-control btn btn-success'] ); ?>
+    </div>
+
+
+    <?php echo Form::close(); ?>
+
+
+</div>
+</body>
+</html>
---
+++ src/Form.php
@@ -0,0 +1,236 @@
+<?php
+
+namespace mirac\BasicForm;
+
+/**
+ * Class Form
+ * 
+ * @author Miraç Şengönül
+ *         @mail miracsengonul@gmail.com
+ */
+
+Class Form{
+
+
+    /**
+     * Form Open Tag
+     * Form::open('Route','Method',Array)
+     * @param string $action
+     * @param string $method
+     * @param array|NULL $ozel
+     * @return string
+     */
+	static public function open($action,$method,array $ozel=NULL){
+	    if($ozel != NULL) {
+            foreach ($ozel as $key => $value) {
+                $ac_array[] = $key . '="' . $value . '"';
+            }
+            $ac_array       = implode(' ', $ac_array);
+        }else{
+            $ac_array       = NULL;
+        }
+		return  '<form action="'.$action.'" method="'.$method.'" '.$ac_array.'>';
+	}
+
+
+
+    /**
+     * Form Input Generate
+     * /Form::input(Array)
+     * @param array|NULL $ozel
+     * @return string
+     */
+    static public function input(array $ozel=NULL){
+        if($ozel != NULL) {
+            foreach ($ozel as $key => $value) {
+                $input_array[] = $key . '="' . $value . '"';
+            }
+            $input_array       = implode(' ', $input_array);
+        }else{
+            $input_array        = NULL;
+        }
+        return  '<input '.$input_array.'>';
+    }
+
+
+
+    /**
+     * Form Input Date
+     * Form::date(Name,Array)
+     * @param $name
+     * @param array|NULL $ozel
+     * @return string
+     */
+    static public function date($name,array $ozel=NULL){
+        if($ozel != NULL) {
+            foreach ($ozel as $key => $value) {
+                $input_date_array[] = $key . '="' . $value . '"';
+            }
+            $input_date_array       = implode(' ', $input_date_array);
+        }else{
+            $input_date_array       = NULL;
+        }
+        return  '<input type="date" name="'.$name.'" '.$input_date_array.'>';
+    }
+
+
+
+    /**
+     * Form Input Mail
+     * Form::mail(Name,Array)
+     * @param $name
+     * @param array|NULL $ozel
+     * @return string
+     */
+    static public function mail($name,array $ozel=NULL){
+        if($ozel != NULL) {
+            foreach ($ozel as $key => $value) {
+                $input_mail_array[] = $key . '="' . $value . '"';
+            }
+            $input_mail_array       = implode(' ', $input_mail_array);
+        }else{
+            $input_mail_array       = NULL;
+        }
+        return  '<input type="mail" name="'.$name.'" '.$input_mail_array.'>';
+    }
+
+
+
+    /**
+     * Form Input Password
+     * Form::pass(Name,Array)
+     * @param $name
+     * @param array|NULL $ozel
+     * @return string
+     */
+    static public function pass($name,array $ozel=NULL){
+        if($ozel != NULL) {
+            foreach ($ozel as $key => $value) {
+                $input_pass_array[] = $key . '="' . $value . '"';
+            }
+            $input_pass_array       = implode(' ', $input_pass_array);
+        }else{
+            $input_pass_array       = NULL;
+        }
+        return  '<input type="password" name="'.$name.'" '.$input_pass_array.'>';
+    }
+
+
+
+    /**
+     * Form Input Text
+     * Form::text(Name,Array)
+     * @param $name
+     * @param array|NULL $ozel
+     * @return string
+     */
+    static public function text($name,array $ozel=NULL){
+        if($ozel != NULL) {
+            foreach ($ozel as $key => $value) {
+                $input_text_array[] = $key . '="' . $value . '"';
+            }
+            $input_text_array       = implode(' ', $input_text_array);
+        }else{
+            $input_text_array       = NULL;
+        }
+        return  '<input type="text" name="'.$name.'" '.$input_text_array.'>';
+    }
+
+
+    /**
+     * Forum Submit Input
+     * Form::submit(Name,Value,Array)
+     * @param $name
+     * @param $value
+     * @param array|NULL $ozel
+     * @return string
+     */
+    static public function submit($name,$deger,array $ozel=NULL){
+        if($ozel != NULL) {
+            foreach ($ozel as $key => $value) {
+                $input_submit_array[] = $key . '="' . $value . '"';
+            }
+            $input_submit_array       = implode(' ', $input_submit_array);
+        }else{
+            $input_submit_array       = NULL;
+        }
+        return  '<input type="submit" value="'.$deger.'" name="'.$name.'" '.$input_submit_array.'>';
+    }
+
+
+    /**
+     * Textarea Generate
+     * Form::textarea(Name,Array)
+     * @param $name
+     * @param array|NULL $ozel
+     * @return string
+     */
+    static public function textarea($name,array $ozel=NULL){
+        if($ozel != NULL) {
+            foreach ($ozel as $key => $value) {
+                $textarea_array[] = $key . '="' . $value . '"';
+            }
+            $textarea_array       = implode(' ', $textarea_array);
+        }else{
+            $textarea_array       = NULL;
+        }
+        return  '<textarea name="'.$name.'" '.$textarea_array.'></textarea>';
+    }
+
+
+    /**
+     * Select Input
+     * @param $name
+     * @param array $options
+     * @param array|NULL $ozel
+     * @return string
+     */
+    static public function select($name,array $options,array $ozel=NULL){
+        if($ozel != NULL) {
+            foreach ($ozel as $key => $value) {
+                $select_array[] = $key . '="' . $value . '"';
+            }
+            $select_array       = implode(' ', $select_array);
+        }else{
+            $select_array       = NULL;
+        }
+
+        $select = '<select name="'.$name.'" '.$select_array.'>';
+        foreach ($options as $key=>$option){
+           $select .=  '<option value="'.$key.'">'.$option.'</option>';
+        }
+        $select .= '</select>';
+        return  $select;
+    }
+
+    /**
+     * Label Generate
+     * @param $name
+     * @param $for
+     * @param array|NULL $ozel
+     * @return string
+     */
+    static public function label($name,$for,array $ozel=NULL){
+        if($ozel != NULL) {
+            foreach ($ozel as $key => $value) {
+                $label_array[] = $key . '="' . $value . '"';
+            }
+            $label_array       = implode(' ', $label_array);
+        }else{
+            $label_array       = NULL;
+        }
+        return  '<label for="'.$for.'" '.$label_array.'>'.$name.'</label>';
+    }
+
+    /**
+     * Form Close Tag
+     * Form::close()
+     * @return string
+     */
+	static public function close(){
+	    return '</form>';
+	}
+
+
+
+}
